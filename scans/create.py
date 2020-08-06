bd17b88f059015a52112bf4a5c8b11e6a5f09cc8
Alessandro Braidotti <alessandro@kintyre.co>
2020-08-06 15:35:24 -0400

    add compileable, deployable, but untested serverless api

--- scans/create.py
+++ scans/create.py
@@ -2,7 +2,7 @@ import json
 import logging
 import uuid
 
-from scans.scan_model import TodoModel
+from scans.scan_model import ScanModel
 
 
 def create(event, context):
@@ -17,14 +17,13 @@ def create(event, context):
         return {'statusCode': 422,
                 'body': json.dumps({'error_message': 'Couldn\'t create the scan item. No text.'})}
 
-    a_todo = ScanModel(user_id=event.path.user_id,
-                        scan_id=str(uuid.uuid1()),
-                        text=data['text'])
+    a_scan = ScanModel(user_id=data['user_id'],
+                       scan_id=str(uuid.uuid1()),
+                       text=data['text'])
 
-    # write the todo to the database
+    # write the scan to the database
     a_scan.save()
 
     # create a response
     return {'statusCode': 201,
             'body': json.dumps(dict(a_scan))}
-
